d9687efd09547d11dde747b8e77635e4dcd3260e
Mayra del Valle González <70923438+MayradelvGonzalez@users.noreply.github.com>
2025-05-18 15:35:51 -0300

    elementosEinfo

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ContactSection = React.forwardRef((_, ref) => {
+  const [formData, setFormData] = useState({ name: '', message: '' });
+  const [messageSent, setMessageSent] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const mensajeWhatsApp = `Nuevo mensaje de contacto:\n\nNombre: ${formData.name}\nMensaje: ${formData.message}`;
+    const urlWhatsApp = `https://wa.me/+5493815160008?text=${encodeURIComponent(mensajeWhatsApp)}`;
+
+    window.open(urlWhatsApp, '_blank');
+    setMessageSent(true); // Mostrar el mensaje de éxito
+    setTimeout(() => {
+      setMessageSent(false); // Ocultar el mensaje de éxito después de unos segundos
+      setFormData({ name: '', message: '' }); // Limpiar el formulario
+    }, 3000); // La alerta se ocultará después de 3 segundos (ajusta el tiempo si lo deseas)
+  };
+
   return (
     <section
       className="py-20 bg-white"
@@ -10,25 +31,19 @@ const ContactSection = React.forwardRef((_, ref) => {
       <div className="container mx-auto px-4 max-w-4xl">
         <h2 className="text-3xl font-bold text-green-800 mb-12 text-center">Contacto</h2>
 
-        <div className="bg-green-50 rounded-xl p-8 shadow-lg">
-          <form className="space-y-6">
+        <div className="bg-green-50 rounded-xl p-8 shadow-lg relative"> {/* Añadido relative para posicionar el mensaje */}
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name" className="block text-black-800 font-medium mb-2">Nombre</label>
               <input
                 type="text"
                 id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
                 className="w-full px-4 py-3 rounded-lg border border-black-200 focus:outline-none focus:ring-2 focus:ring-green-500"
                 placeholder="Tu nombre"
-              />
-            </div>
-
-            <div>
-              <label htmlFor="email" className="block text-black-800 font-medium mb-2">Email</label>
-              <input
-                type="email"
-                id="email"
-                className="w-full px-4 py-3 rounded-lg border border-black-200 focus:outline-none focus:ring-2 focus:ring-green-500"
-                placeholder="tu@email.com"
+                required
               />
             </div>
 
@@ -36,20 +51,32 @@ const ContactSection = React.forwardRef((_, ref) => {
               <label htmlFor="message" className="block text-black-800 font-medium mb-2">Mensaje</label>
               <textarea
                 id="message"
+                name="message"
                 rows="4"
+                value={formData.message}
+                onChange={handleChange}
                 className="w-full px-4 py-3 rounded-lg border border-green-200 focus:outline-none focus:ring-2 focus:ring-green-500"
                 placeholder="Hola, me gustaría contactarte acerca de..."
+                required
               ></textarea>
             </div>
 
             <button
               type="submit"
-              className="w-full hover:bg-green-700 text-black font-bold py-3 px-6 rounded-lg transition duration-300"
+              className="w-full text-black font-bold py-3 px-6 rounded-lg transition duration-300 hover:text- hover:bg-[#FFC107]"
               style={{ backgroundColor: '#5d7c66' }}
             >
-              Enviar mensaje
+              Enviar mensaje por WhatsApp <img src="https://img.icons8.com/ios-filled/50/ffffff/whatsapp.png" alt="WhatsApp" className="inline-block w-5 h-5 ml-2" />
             </button>
           </form>
+
+          {messageSent && (
+            <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+              <div className="bg-white p-6 rounded-md shadow-lg text-center">
+                <p className="text-lg font-semibold text-green-600">Tu mensaje ha sido preparado para ser enviado por WhatsApp.</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </section>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,25 +1,57 @@
 import React from 'react';
+import remoteWorkGraphic from '../imagen.png'; // Importa la imagen (ajusta la ruta)
 
-const HeroSection = ({ scrollToContact }) => { // Recibir la función como prop
+const HeroSection = ({ scrollToContact }) => {
   return (
-    <section className="min-h-screen bg-gradient-to-b from-green-50 to-white flex items-center justify-center px-4">
-      <div className="text-center max-w-4xl">
-        <h1 className="text-5xl md:text-7xl font-bold text-green-800 mb-6">
-          Hola, soy <span className="text-green-600">FullStack MERN</span>
-        </h1>
-        <p className="text-xl md:text-2xl text-gray-700 mb-8">
-          Junior Developer Jr & Project Manager Trainee
-        </p>
-        <div className="flex gap-4 justify-center">
-          <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-8 rounded-full transition-all duration-300 transform hover:scale-105 hover:text-black hover:bg-red-400" style={{ border: '1px solid black' }}>
-            Ver proyectos
-          </button>
-          <button
-            className="bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-8 rounded-full transition-all duration-300 transform hover:scale-105 hover:text-black hover:bg-red-400" style={{ border: '1px solid black' }}
-            onClick={scrollToContact} // Usar la función recibida como prop
-          >
-            Contacto
-          </button>
+    <section className="min-h-screen bg-gradient-to-b from-green-50 to-white flex items-center justify-center px-4"
+           style={{
+    backgroundColor: '#e5e5f7',
+    opacity: 0.8,
+    backgroundImage: 'repeating-radial-gradient( circle at 0 0, transparent 0, #e5e5f7 10px ), repeating-linear-gradient(rgba(45, 175, 153, 0.33),rgb(17, 99, 41) )',
+  }}
+    
+    
+    
+    
+    >
+      <div className="flex flex-col md:flex-row items-center justify-center max-w-5xl" 
+
+      style={{ backgroundColor: 'lightgray', padding: '86px', borderRadius: '20px' }} // 
+      
+      >
+        {/* Imagen a la izquierda (se mostrará en la parte superior en pantallas pequeñas) */}
+        <img
+          src={remoteWorkGraphic}
+          alt="Ilustración de trabajo remoto o diseño"
+          className="w-64 h-auto mb-6 md:mb-0 md:mr-8" // Ajusta el tamaño y los márgenes según necesites
+        />
+
+        {/* Contenido de texto a la derecha */}
+        <div className="text-center md:text-left">
+          <h1 className="text-5xl md:text-7xl font-bold text-green-900 mb-6">
+            Hola, soy <span className="text-green-900">Mayra</span>
+          </h1>
+          <p className="text-xl md:text-2xl text-black-900 mb-8">
+            Junior Developer Jr & Project Manager Trainee
+          </p>
+          <div className="flex gap-4 justify-center md:justify-start">
+            <a
+              href="https://github.com/MayradelvGonzalez"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-green-600 hover:bg-green-700 text-black font-bold py-3 px-8 rounded-full transition-all duration-300 transform hover:scale-105 hover:text-black hover:bg-red-400"
+              style={{ border: '1px solid black', display: 'inline-block', textDecoration: 'none' }}
+            >
+              Proyectos
+            </a>
+            <button
+              className="bg-green-600 hover:bg-green-700 text-black font-bold py-3 px-8 rounded-full transition-all duration-300 transform hover:scale-105 hover:text-black hover:bg-red-400"
+              style={{ border: '1px solid black' }}
+              onClick={scrollToContact} // Usar la función recibida como prop
+            >
+              Contacto
+            </button>
+          </div>
         </div>
       </div>
     </section>
